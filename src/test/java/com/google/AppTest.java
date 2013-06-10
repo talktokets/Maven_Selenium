acdd42a2250a8d0ec6d037c6a00531647305215f
Omkar Khatavkar <omkar.k@vertisinfotech.com>
2013-06-10 14:29:41 +0530

    Added mavenized simple selenium script.

---
+++ src/test/java/com/google/AppTest.java
@@ -0,0 +1,26 @@
+package com.google;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class AppTest 
+    
+{
+	WebDriver driver;
+
+	@Test(description="This will open browser with google.com",invocationCount=3)
+	public void Test1(){
+		
+		driver = new FirefoxDriver();
+		driver.get("http://www.google.com");
+		driver.quit();
+			}
+		
+	  
+	
+
+}
